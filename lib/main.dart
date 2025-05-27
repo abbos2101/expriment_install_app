c34d613aedaa88cdf09d02e465a4a8edaef87393
abbos <abbos2101@gmail.com>
2025-05-27 14:57:01 +0500

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ota_update/ota_update.dart';
 
 void main() => runApp(MyApp());
@@ -25,18 +26,18 @@ class _MyAppState extends State<MyApp> {
   Future<void> tryOtaUpdate() async {
     try {
       print('ABI Platform: ${await OtaUpdate().getAbi()}');
-      //LINK CONTAINS APK OF FLUTTER HELLO WORLD FROM FLUTTER SDK EXAMPLES
       OtaUpdate()
           .execute(
         'https://raw.githubusercontent.com/abbos2101/expriment_install_app/refs/heads/main/app_versions/app-release.apk',
-        // destinationFilename: 'flutter_hello_world.apk',
-        //FOR NOW ANDROID ONLY - ABILITY TO VALIDATE CHECKSUM OF FILE:
         sha256checksum:
-        '2124562199071c974d1919373c712fb707c9dc8e1245bbc7de122c6ad33637dc',
+        '477efcc353738688b0a02d27cc0dcedea112dd70e5259f79cd6ef8203b0a2d44',
       )
           .listen(
             (OtaEvent event) {
           setState(() => currentEvent = event);
+          if (event.status == OtaStatus.INSTALLING) {
+            SystemNavigator.pop();
+          }
         },
       );
       // ignore: avoid_catches_without_on_clauses
