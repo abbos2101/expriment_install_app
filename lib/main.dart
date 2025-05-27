a93988f13c5c2697bbfae9a0868c0de1e053cc4d
abbos <abbos2101@gmail.com>
2025-05-27 15:09:02 +0500

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,17 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:ota_update/ota_update.dart';
+
+import 'update_page.dart';
 
 void main() => runApp(MyApp());
 
-/// example widget for ota_update plugin
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  OtaEvent? currentEvent;
-
-  @override
-  void initState() {
-    super.initState();
-    tryOtaUpdate();
-  }
-
-  Future<void> tryOtaUpdate() async {
-    try {
-      print('ABI Platform: ${await OtaUpdate().getAbi()}');
-      OtaUpdate()
-          .execute(
-        'https://raw.githubusercontent.com/abbos2101/expriment_install_app/refs/heads/main/app_versions/app-release.apk',
-        sha256checksum:
-        '477efcc353738688b0a02d27cc0dcedea112dd70e5259f79cd6ef8203b0a2d44',
-      )
-          .listen(
-            (OtaEvent event) {
-          setState(() => currentEvent = event);
-          if (event.status == OtaStatus.INSTALLING) {
-            SystemNavigator.pop();
-          }
-        },
-      );
-      // ignore: avoid_catches_without_on_clauses
-    } catch (e) {
-      print('Failed to make OTA update. Details: $e');
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (currentEvent == null) {
-      return Container();
-    }
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('OTA status: ${currentEvent?.status} : ${currentEvent?.value} \n'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: UpdatePage(),
     );
   }
 }
---
+++ lib/update_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ota_update/ota_update.dart';
+
+class UpdatePage extends StatefulWidget {
+  const UpdatePage({super.key});
+
+  @override
+  State<UpdatePage> createState() => _UpdatePageState();
+}
+
+class _UpdatePageState extends State<UpdatePage> {
+  OtaEvent? currentEvent;
+  final sha256controller = TextEditingController();
+
+  Future<void> tryOtaUpdate() async {
+    if (sha256controller.text.isEmpty) return;
+
+    try {
+      print('ABI Platform: ${await OtaUpdate().getAbi()}');
+      OtaUpdate()
+          .execute(
+            'https://raw.githubusercontent.com/abbos2101/expriment_install_app/refs/heads/main/app_versions/app-release.apk',
+            sha256checksum: sha256controller.text,
+          )
+          .listen((OtaEvent event) {
+            setState(() => currentEvent = event);
+            if (event.status == OtaStatus.INSTALLING) {
+              SystemNavigator.pop();
+            }
+          });
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e) {
+      print('Failed to make OTA update. Details: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    sha256controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        floatingActionButton: FloatingActionButton(onPressed: tryOtaUpdate),
+        body: Center(
+          child: Column(
+            spacing: 6,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'OTA status: ${currentEvent?.status} : ${currentEvent?.value} \n',
+              ),
+              TextField(controller: sha256controller),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
